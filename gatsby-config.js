782c9e3254d0beb5602b295cc5e35fc502efb4fe
Vincenzo Delre <delrealberto@gmail.com>
2022-03-10 19:55:03 -0300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,32 @@ module.exports = {
       title: ``,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [{
+  plugins: [
+    {resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts the following options, all of which are defined by `@emotion/babel-plugin` plugin.
+        // The values for each key in this example are the defaults the plugin uses.
+        sourceMap: true,
+        autoLabel: "dev-only",
+        labelFormat: `[local]`,
+        cssPropOptimization: true,
+      }
+    },
+    {
     resolve: 'gatsby-source-datocms',
     options: {
       "apiToken": "c7a92b3752088b22343ff02ea29a07"
     }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+  }, "gatsby-plugin-image",
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/images`,
+    },
+  }, "gatsby-plugin-react-helmet",
+  
+  "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
