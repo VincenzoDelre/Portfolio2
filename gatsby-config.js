f8cb49841ed7972e317173205d533adc639ee2f4
Vincenzo Delre <delrealberto@gmail.com>
2022-03-12 09:56:37 -0300

    react-water-wave6

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,13 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
+    
     {resolve: `gatsby-plugin-emotion`,
       options: {
         // Accepts the following options, all of which are defined by `@emotion/babel-plugin` plugin.
         // The values for each key in this example are the defaults the plugin uses.
         sourceMap: true,
-        autoLabel: "dev-only",
+        autoLabel: "always",
         labelFormat: `[local]`,
         cssPropOptimization: true,
       }
